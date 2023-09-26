3d4a897843e07b62898cdb9cfcc951c7fd8a25bf
raikumar-lamzing <raikumar.khangembam@lamzing.com>
2023-09-26 15:22:22 +0530

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+    public function index(){
+        return view("index");
+    }
+    public function about(){
+        return view("pages.about");
+    }
+
+    public function products(){
+        return view("pages.products");
+    }
+}
